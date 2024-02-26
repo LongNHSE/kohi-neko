9202638edc32564e742836f6ef776514940936b5
Oalskad <53299953+Oalskad@users.noreply.github.com>
2024-02-26 21:33:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -36,8 +36,7 @@ const testRouter = require('./routes/testRouter');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(cors());
 app.use(logger('dev'));
