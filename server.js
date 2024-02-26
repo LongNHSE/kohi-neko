b6df8678a01439b9428bf260649438f4df3e7159
Oalskad <53299953+Oalskad@users.noreply.github.com>
2024-02-26 21:42:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const app = require('./app');
+// const app = require('./app');
+const express = require('express');
+
+const app = express();
 const { frontendURL } = require('./utils/urlConstant');
 
 app.use(cors({ origin: frontendURL }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.route('/').get((req, res) => {
+  res.send('Hello from the server side');
+});
 dotenv.config({ path: './config.env' });
 const port = process.env.PORT || 8000;
 // const DB = process.env.LOCAL_DATABASE_URI;
