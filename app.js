6066244cbfce140523032425e59f4728c660ecd7
Nguyen Huy Long <huylong2113@gmail.com>
2024-03-13 14:02:11 +0700

    Remove template engine

--- app.js
+++ app.js
@@ -39,8 +39,6 @@ const adminRouter = require('./routes/adminRouter');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
